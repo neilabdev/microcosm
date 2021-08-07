8b36bbf1a3af5150a1dda8b64962385474b620af
James Whitfield <ghost@neilab.com>
2021-08-07 16:35:55 -0400

    Added inclusion of util.

--- lib/microcosm.rb
+++ lib/microcosm.rb
@@ -1,7 +1,10 @@
 require "microcosm/version"
+require_relative "microcosm/util"
 require_relative "microcosm/cache"
 require_relative "microcosm/database"
+
 module Microcosm
 
 end
+
 require 'microcosm/railtie'  if defined?(Rails::Railtie)
